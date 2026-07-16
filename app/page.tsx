8968030575c764e1e3b3602b9b8de81dff7a7667
abdallahelmadi <abdallahelmadi42@gmail.com>
2026-07-16 11:55:53 +0100

    boobs

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,23 @@ export default function Home(): React.ReactNode {
         className="w-40 h-22.25"
       />
 
-      <h1 className="text-5xl font-medium mt-4">
+      <h1 className="text-5xl font-medium mt-4 -ml-1">
         BOOBS MAKE THINKS GROW
       </h1>
 
-      <h2 className="text-[17px] max-w-175 tracking-tighter">
+      <h2 className="text-[16px] max-w-175 tracking-tighter leading-none">
         We are here to do the same for your business.
-        We are a team of passionate designers and developers who love to create beautiful and functional applications, and solve problems with code.
+        We are a team of passionate designers and developers who love
+        to create beautiful and functional applications, and solve problems with code.
       </h2>
+
+      <button
+        className="bg-black text-[#DE2670] mt-6 w-48 h-12 border border-gray-800
+        font-medium cursor-pointer transition-all duration-300 ease-in-out
+        hover:translate-x-2 hover:translate-y-2 select-none"
+      >
+        Contact us
+      </button>
 
     </main>
   )
